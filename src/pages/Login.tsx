fdc38f720489117bce5c8737d1ed23b60fb44a9d
Maximiliano Albarran <maxialbarran@hotmail.com>
2023-03-03 17:46:22 -0300

    login page updated

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,18 +4,18 @@ import {
   Text,
   VStack,
   useToast,
-  Spinner,
-  Heading,
 } from "@chakra-ui/react";
-import { ChangeEvent, FormEvent, useContext, useState } from "react";
+import { FormEvent, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import CommonInput from "../components/common/input/input";
-import Formulary from "../components/Formulary";
+import Formulary from "../components/common/formBox/formBox";
 
 import { auth } from "../config/firebase";
 import { AuthError, signInWithEmailAndPassword } from "firebase/auth";
-import { AuthContext } from "../context/AuthContext";
+import CommonSpinner from "../components/common/spinner/spinner";
+import { useForm } from "../hooks/Form/useForm";
+import { useShowToast } from "../hooks/Toast/useShowToast";
 
 type User = {
   email: string;
@@ -24,28 +24,17 @@ type User = {
 
 const Login = () => {
   const navigate = useNavigate();
-  const [user, setUser] = useState<User>({ email: "", password: "" });
   const [loading, setLoading] = useState(false);
-  const { isUserActive } = useContext(AuthContext);
-
-  const toast = useToast();
-
-  const setToast = (st: string, message: string) => {
-    toast({
-      title: message,
-      status: st == "error" ? "error" : "success",
-      isClosable: true,
-      position: "top",
-      duration: 2000,
-    });
-  };
+  
+  const {showToast} = useShowToast();
+  const {form, handleChange} = useForm<User>({ email: "", password: "" })
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     try {
       setLoading(true);
-      await signInWithEmailAndPassword(auth, user.email, user.password);
-      setToast("success", "Bienvenido");
+      await signInWithEmailAndPassword(auth, form.email, form.password);
+      showToast({st: "success", label: "Bienvenido!"});
 
       navigate("/home");
       setLoading(false);
@@ -56,68 +45,61 @@ const Login = () => {
           ? "Contraseña incorrecta"
           : "Usuario no encontrado, verifique su direccion de correo";
 
-      setToast("error", message);
+      showToast({st: "error", label: message});
       setLoading(false);
     }
   };
 
-  const handleChange = (e: ChangeEvent) => {
-    const { target } = e;
-
-    setUser({ ...user, [target.name]: target.value });
-  };
-
   return (
-    <Container minW="100%" minH="100vh" display={"grid"} placeContent="center">
-      {isUserActive && (
-        <Heading>Bienvenido {auth.currentUser?.displayName}!</Heading>
-      )}
-      {!isUserActive && (
+    <Container
+      minW="container.lg"
+      display="flex"
+      justifyContent={"center"}
+      py="6"
+      mt={"24"}
+    >
+      {loading ? (
+        <CommonSpinner />
+      ) : (
         <>
-          {loading ? (
-            <Spinner />
-          ) : (
-            <>
-              {" "}
-              <Formulary
-                title="Login"
-                buttonLabel="Login"
-                onSubmit={(event) => handleSubmit(event)}
-              >
-                <VStack spacing="3">
-                  <CommonInput
-                    label="Email"
-                    type="email"
-                    isRequired={false}
-                    name="email"
-                    value={user.email}
-                    onChange={(event) => handleChange(event)}
-                  />
-                  <CommonInput
-                    label="Contraseña"
-                    type="password"
-                    isRequired={false}
-                    name="password"
-                    value={user.password}
-                    onChange={(event) => handleChange(event)}
-                  />
-                </VStack>
+          {" "}
+          <Formulary
+            title="Login"
+            buttonLabel="Login"
+            onSubmit={(event) => handleSubmit(event)}
+          >
+            <VStack spacing="3">
+              <CommonInput
+                label="Email"
+                type="email"
+                isRequired={false}
+                name="email"
+                value={form.email}
+                onChange={(event) => handleChange(event)}
+              />
+              <CommonInput
+                label="Contraseña"
+                type="password"
+                isRequired={false}
+                name="password"
+                value={form.password}
+                onChange={(event) => handleChange(event)}
+              />
+            </VStack>
 
-                <HStack align="center" justify={"center"} py="1" my="5">
-                  <Text color="#999" fontSize="sm">
-                    Aun no tienes una cuenta?
-                  </Text>
-                  <Text
-                    color="blue.700"
-                    fontSize="md"
-                    _hover={{ textDecorationLine: "underline" }}
-                  >
-                    <Link to="auth">Registrate!</Link>
-                  </Text>
-                </HStack>
-              </Formulary>
-            </>
-          )}
+            <HStack align="center" justify={"center"} py="1" my="5">
+              <Text color="#999" fontSize="sm">
+                Aun no tienes una cuenta?
+              </Text>
+              <Text
+                color="blue.800"
+                fontSize="md"
+                _hover={{ textDecorationLine: "underline" }}
+              >
+                <Link to="auth">Registrate!</Link>
+              </Text>
+            </HStack>
+          </Formulary>
         </>
       )}
     </Container>
